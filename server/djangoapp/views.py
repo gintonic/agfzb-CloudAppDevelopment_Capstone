9730b2f4791fa968b7321e44d066e37898eed10e
Thanakorn Sirikulpan <gintonicswingjazz@gmail.com>
2021-10-17 07:59:36 +0000

    add review method

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -116,4 +116,22 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
+
+def add_review(request, dealer_id):
+    if request.method == "POST":
+        review = dict()
+        review["dealership"] = dealership
+        review["name"] = name
+        review["purchase"] = purchase
+        review["id"] = id
+        review["review"] = review
+        review["purchase_date"] = datetime.utcnow().isoformat()
+        review["car_make"] = car_make
+        review["car_model"] = car_model
+        review["car_year"] = car_year
+
+        json_payload = dict()
+        json_payload ["review"] = review
+        response = post_request(url, json_payload, dealerId=dealer_id)
+        return HttpResponse(response)
 
